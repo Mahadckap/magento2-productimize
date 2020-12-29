0891bb44f00f2be4b21c5d688c1f382c7456e998
Mahalakshmi M <mahalakshmim@dckap.com>
2020-12-29 17:33:01 +0530

    Added productimize module

--- registration.php
+++ registration.php
@@ -9,6 +9,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'DCKAP_Productimize',
+    'Mahadckap_Productimize',
     __DIR__
 );
